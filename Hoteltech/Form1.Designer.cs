541d8e271e6a1640af18c06dcdae1f6a38074530
brunosoharagit <b.sohara.git@gmail.com>
2020-02-23 19:10:29 -0300

    Update 2

--- Hoteltech/Form1.Designer.cs
+++ Hoteltech/Form1.Designer.cs
@@ -29,37 +29,59 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txbUser = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txbUser
             // 
-            this.textBox1.ForeColor = System.Drawing.Color.Transparent;
-            this.textBox1.Location = new System.Drawing.Point(144, 176);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(239, 20);
-            this.textBox1.TabIndex = 0;
+            this.txbUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txbUser.BackColor = System.Drawing.Color.AliceBlue;
+            this.txbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUser.ForeColor = System.Drawing.Color.Black;
+            this.txbUser.Location = new System.Drawing.Point(27, 187);
+            this.txbUser.Name = "txbUser";
+            this.txbUser.PasswordChar = '•';
+            this.txbUser.Size = new System.Drawing.Size(250, 26);
+            this.txbUser.TabIndex = 1;
             // 
-            // textBox2
+            // btnEnter
             // 
-            this.textBox2.Location = new System.Drawing.Point(144, 249);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(239, 20);
-            this.textBox2.TabIndex = 1;
+            this.btnEnter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEnter.BackColor = System.Drawing.Color.LightCyan;
+            this.btnEnter.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.ForeColor = System.Drawing.Color.Black;
+            this.btnEnter.Location = new System.Drawing.Point(64, 254);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(190, 30);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Entrar";
+            this.btnEnter.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // txbPassword
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(169, 310);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Entrar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.txbPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txbPassword.BackColor = System.Drawing.Color.AliceBlue;
+            this.txbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbPassword.ForeColor = System.Drawing.Color.Black;
+            this.txbPassword.Location = new System.Drawing.Point(27, 118);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(250, 26);
+            this.txbPassword.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.txbPassword);
+            this.panel1.Controls.Add(this.btnEnter);
+            this.panel1.Controls.Add(this.txbUser);
+            this.panel1.Location = new System.Drawing.Point(108, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 302);
+            this.panel1.TabIndex = 3;
             // 
             // frmLogin
             // 
@@ -68,22 +90,22 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(784, 465);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Name = "frmLogin";
             this.Text = "Hoteltech - Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txbUser;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
